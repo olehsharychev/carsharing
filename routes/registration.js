7eada1af9d4cba12eca46f181fa22d688b95b367
Xike121 <xike121@gmail.com>
2018-10-18 22:19:22 +0300

    registration added

--- routes/registration.js
+++ routes/registration.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
+var bcrypt = require('bcrypt');
 var con = require('../connect/connection.js');
 
-var carsList;
-
-// con.query("SELECT * FROM test_table", function (err, result, fields) {
-//     if (err) throw err;
-//     console.log(result);
-//     carsList = result;
-// });
+const saltRounds = 10;
 
 router.get('/', function (req, res, next) {
     res.render('registration');
@@ -17,13 +11,20 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res) {
     if (!req.body) return res.sendStatus(400);
-    console.log(req.body.login);
-    var query = "INSERT INTO `user` (`user_id`, `user_role`, `user_login`, `user_password`) VALUES "
-                + "(NULL, 'renter', '" + req.body.login + "', '1111');";
-    console.log(query);
-    con.query(query, function (err) {
+
+    //получаем хеш пароля
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
         if (err) throw err;
+
+        //записываем роль, логин и хеш пароля в БД
+        var query = "INSERT INTO `user` (`user_id`, `user_role_id`, `user_login`, `user_password`) VALUES "
+            + "(NULL, '" + req.body['role[]'] + "', '" + req.body.login + "', '" + hash +"');";
+
+        con.query(query, function (err) {
+            if (err) throw err;
+        });
     });
+
     res.redirect('/cars');
 });
 
