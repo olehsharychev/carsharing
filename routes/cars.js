3d2e1f2c1f0084bd2790f20cee9cfa873788b2b2
Xike121 <shar1997@ukr.net>
2019-05-10 21:41:04 +0300

    search added

--- routes/cars.js
+++ routes/cars.js
@@ -20,4 +20,23 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/search', function (req, res, next) {
+    console.log(req.query);
+    var searchQuery = `SELECT * FROM ad, image WHERE 
+                       ad.ad_id = image.ad_id
+                       AND
+                       ad_tittle LIKE '%${req.query.searchData}%'
+                       GROUP BY ad.ad_id`;
+    con.query(searchQuery, function (err, result) {
+        if (err) throw err;
+        var currentUser = 0;
+        var currentRole = 0;
+        if (req.user !== undefined){
+            currentUser = req.user.user_id;
+            currentRole = req.user.user_role_id;
+        }
+        res.render('cars', {adList: result, currentUser: currentUser, currentRole: currentRole})
+    });
+});
+
 module.exports = router;
